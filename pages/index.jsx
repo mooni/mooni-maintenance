adba17f43b7018923a88b4bd39f3a87d6fb238c8
Pacien Boisson <pakokrew@gmail.com>
2021-01-23 18:58:11 +0400

    maintenance page

---
+++ pages/index.jsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="./logo512.png" className={styles.logo}/>
+        <h1 className={styles.title}>
+          Mooni
+        </h1>
+
+        <p className={styles.description}>
+          Mooni is under maintenance. We will be back soon !
+        </p>
+      </main>
+    </div>
+  )
+}
